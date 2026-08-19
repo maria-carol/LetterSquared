7a1587ff221af206215af450193bf5a27b94f14f
Maria Carolina <95097678+maria-carol@users.noreply.github.com>
2026-08-19 00:33:08 +0000

    First commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.title("LetterSquared")
+st.subheader(
+    "Sua plataforma preferida para filmes, reviews e informações que são 'Absolute CINEMA'!"
+    , divider=True
 )
