d38adc24e57392d563f6fa8b90f70439efc0ad30
Maria Carolina <95097678+maria-carol@users.noreply.github.com>
2026-08-20 01:10:55 +0000

    feat: configuracao inicial do app e conexao firestore

---
+++ pages/01_Firebase.py
@@ -0,0 +1,24 @@
+import streamlit as st
+from google.cloud import firestore
+
+#Conectando a base de dados
+dataBase = firestore.Client.from_service_account_json("firebase.json")
+
+st.title("Inserir novo filme na Base de dados")
+
+# --- Formulário de cadastro de filmes
+with st.form("formFirebase"):
+    titulo = st.text_input("Título:", placeholder="Informe o título...")
+    genero = st.text_input("Gênero:", placeholder="Informe o gênero principal...")
+    diretor = st.text_input("Diretor:", placeholder="Informe o diretor...")
+    rating = st.number_input("Avaliação:", placeholder="Insira a nota média de avaliação...")
+
+    btnSalvarFilme = st.form_submit_button("Salvar", use_container_width=True)
+
+    if btnSalvarFilme:
+        if titulo and genero and diretor and rating:
+            #Salvar no banco
+            novoFilme = dataBase.collection("filmes").document()
+            st.success("Filme adicionado!")
+        else:
+            st.error("Informe título, genero, diretor e avaliação!")
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,19 @@
 import streamlit as st
 
-st.title("LetterSquared")
+# CABEÇALHO 
+
+#Criando colunas para img + título
+col_logo, col_titulo = st.columns([1, 5], vertical_alignment="center")
+
+#Logotipo
+with col_logo:
+    st.image("img/logotipo.png", width=120)
+
+#Título e subtítulo
+with col_titulo:
+    st.title("LetterSquared")
+
 st.subheader(
-    "Sua plataforma preferida para filmes, reviews e informações que são 'Absolute CINEMA'!"
-    , divider=True
+    "Sua plataforma preferida para filmes, reviews e informações que são 'Absolute CINEMA'!", 
+    divider=True
 )
